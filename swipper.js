612828d6878f7f7f38f9264d511974e9d773599c
Shinu <rajushinu007@gmail.com>
2021-05-16 19:27:22 +0530

    cleared the js file error

--- swipper.js
+++ swipper.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("blogSlides");
-  var dots = document.getElementsByClassName("blogSlider__dots--dot");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -22,9 +22,6 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
